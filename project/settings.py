3d7407447a550bd7788d98f0f2c0ad826ea909ee
Yosra-Hamdi <hamdiyosra010@gmail.com>
2025-03-17 13:07:11 +0100

    Update ALLOWED_HOSTS to allow all hosts and clean up middleware formatting

--- project/settings.py
+++ project/settings.py
@@ -62,11 +62,7 @@ SECRET_KEY = 'django-insecure-ondihgkix58u(acq(*mu$wa)9_qc=tz_d$=rfvnppk7a0nvn8q
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = [
-    'app-flutter-django.onrender.com',
-    'localhost',  # Pour le développement local
-    '127.0.0.1',  # Pour le développement local
-]
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
@@ -102,7 +98,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-   
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
